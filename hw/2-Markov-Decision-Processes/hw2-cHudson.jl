2e9195964922855ca11bb27f0b56bb6c34951267
collin ubuntu <collinhudson.app@gmail.com>
2024-02-06 20:40:56 -0700

    problem 4 score 7

--- hw/2-Markov-Decision-Processes/hw2-cHudson.jl
+++ hw/2-Markov-Decision-Processes/hw2-cHudson.jl
@@ -1,31 +1,20 @@
-# ASEN-5264 Homework 2 Collin Hudson 2/5/2024
+# ASEN-5264 Homework 2 Collin Hudson 2/6/2024
 using DMUStudent.HW2
 using POMDPs: states, actions
 using POMDPTools: ordered_states
 using LinearAlgebra
-##############
-# Instructions
-##############
-#=
-
-This starter code is here to show examples of how to use the HW2 code that you
-can copy and paste into your homework code if you wish. It is not meant to be a
-fill-in-the blank skeleton code, so the structure of your final submission may
-differ from this considerably.
-
-=#
 
 ############
 # Question 3
 ############
 
-function value_iteration(m,discount)
+function value_iteration(m,discount,sprc)
     # It is good to put performance-critical code in a function: https://docs.julialang.org/en/v1/manual/performance-tips/
 
     V = rand(length(states(m)))
     Vprime = rand(length(states(m)))
     R = reward_vectors(m)
-    T = transition_matrices(m)
+    T = transition_matrices(m,sparse=sprc)
     # put your value iteration code here
     ep = 1e-7
     temp = Array{Float64, 2}(undef,length(states(m)),length(actions(m)))
@@ -39,7 +28,7 @@ function value_iteration(m,discount)
     end
     return V
 end
-V = value_iteration(grid_world,0.95)
+V = value_iteration(grid_world,0.95,false)
 # You can use the following commented code to display the value. If you are in an environment with multimedia capability (e.g. Jupyter, Pluto, VSCode, Juno), you can display the environment with the following commented code. From the REPL, you can use the ElectronDisplay package.
     # display(render(grid_world, color=reshape(V[1:100],(10,10))))
 display(render(grid_world, color=V))
@@ -49,14 +38,17 @@ display(render(grid_world, color=V))
 ############
 
 # You can create an mdp object representing the problem with the following:
-m = UnresponsiveACASMDP(2)
+m = UnresponsiveACASMDP(7)
 
 # transition_matrices and reward_vectors work the same as for grid_world, however this problem is much larger, so you will have to exploit the structure of the problem. In particular, you may find the docstring of transition_matrices helpful:
 display(@doc(transition_matrices))
 
-V = value_iteration(m)
+V = value_iteration(m,0.99,true)
 
 @show HW2.evaluate(V)
+
+HW2.evaluate(V, "collin.hudson@colorado.edu")
+
 
 ########
 # Extras
@@ -68,15 +60,15 @@ V = value_iteration(m)
 
 # To convert from physical states to indices in the transition function, use the stateindex function
 # IMPORTANT NOTE: YOU ONLY NEED TO USE STATE INDICES FOR THIS ASSIGNMENT, using the states may help you make faster specialized code for the ACAS problem, but it is not required
-using POMDPs: states, stateindex
+# using POMDPs: states, stateindex
 
-s = first(states(m))
-@show si = stateindex(m, s)
+# s = first(states(m))
+# @show si = stateindex(m, s)
 
-# To convert from a state index to a physical state in the ACAS MDP, use convert_s:
-using POMDPs: convert_s
+# # To convert from a state index to a physical state in the ACAS MDP, use convert_s:
+# using POMDPs: convert_s
 
-@show s = convert_s(ACASState, si, m)
+# @show s = convert_s(ACASState, si, m)
 
-# To visualize a state in the ACAS MDP, use
-render(m, (s=s,))
+# # To visualize a state in the ACAS MDP, use
+# render(m, (s=s,))
