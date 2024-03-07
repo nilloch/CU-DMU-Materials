9a5d44e8a2c41aaf88ed4bc5587712f67689820c
collin ubuntu <collinhudson.app@gmail.com>
2024-03-07 15:34:26 -0700

    sarsa and polgrad

--- hw/4-Tabular-Reinforcement-Learning/hw4-cHudson.jl
+++ hw/4-Tabular-Reinforcement-Learning/hw4-cHudson.jl
@@ -91,7 +91,7 @@ function plotEnv(env,episodes,stop=50000)
             end
         else
             xs = [0.0]
-            thetas = Dict((s) => 0.5*ones(4) for s in RL.observations(env))
+            thetas = Dict((s) => 420*ones(4) for s in RL.observations(env))
             ys = [mean(evaluate(env, s->eps[1].policy(s,thetas)))]
             for i in n:n:min(stop, length(eps))
                 newsteps = sum(length(ep.hist) for ep in eps[i-n+1:i])
@@ -102,7 +102,36 @@ function plotEnv(env,episodes,stop=50000)
         end    
         plot!(p, xs, ys, label=name)
     end
-    p
+    display(p)
+end
+function plotEnvTime(env,episodes,stop=50000)
+    p2 = plot(xlabel="time in environment", ylabel="avg return")
+    n = 1000
+    for (name, eps) in episodes
+        if(name == "SARSA")
+            Q = Dict((s, a) => 0.0 for s in RL.observations(env), a in RL.actions(env))
+            xs = [0.0]
+            ys = [mean(evaluate(env, s->argmax(a->Q[(s, a)], RL.actions(env))))]
+            for i in n:n:min(stop, length(eps))
+                newsteps = sum(length(ep.time) for ep in eps[i-n+1:i])
+                push!(xs, last(xs) + newsteps)
+                Q = eps[i].Q
+                push!(ys, mean(evaluate(env, s->argmax(a->Q[(s, a)], RL.actions(env)))))
+            end
+        else
+            xs = [0.0]
+            thetas = Dict((s) => 420*ones(4) for s in RL.observations(env))
+            ys = [mean(evaluate(env, s->eps[1].policy(s,thetas)))]
+            for i in n:n:min(stop, length(eps))
+                newsteps = sum(length(ep.time) for ep in eps[i-n+1:i])
+                push!(xs, last(xs) + newsteps)
+                thetas = eps[i].thetas
+                push!(ys, mean(evaluate(env, s->eps[i].policy(s,thetas))))
+            end
+        end    
+        plot!(p2, xs, ys, label=name)
+    end
+    display(p2)
 end
 function getAIdx(a)
     idx = 1
@@ -134,59 +163,42 @@ function policyGrad_episode!(thetas,env,alpha,eps=0.1)
         return RL.actions(env)[sampleAction(softMax)]
     end
     function policyEps(s)
-        if rand() < eps
-            return rand(RL.actions(env))
-            # if(s[2] < 9)
-            #     return RL.actions(env)[3]
-            # elseif (s[1] < 9)
-            #     return RL.actions(env)[1]
-            # else
-            #     return rand(RL.actions(env))
-            # end
-        else
+        # if rand() < eps
+        #     return rand(RL.actions(env))
+        # else
             return policy(s,thetas)
-        end
+        # end
     end
     
-    function pgFunc(x)
-        idx = getAIdx(x[2])
-        if(idx == 1)
-            return [1/thetas[x[1]][idx],0.0,0.0,0.0]
-        elseif(idx == 2)
-            return [0.0,1/thetas[x[1]][idx],0.0,0.0]
-        elseif(idx == 3)
-            return [0.0,0.0,1/thetas[x[1]][idx],0.0]
-        elseif(idx == 4)
-            return [0.0,0.0,0.0,1/thetas[x[1]][idx]]
-        end
-    end
     # function pgFunc(x)
     #     idx = getAIdx(x[2])
     #     if(idx == 1)
-    #         return [1/thetas[x[1]][idx],-1/thetas[x[1]][idx],-1/thetas[x[1]][idx],-1/thetas[x[1]][idx]]
+    #         return [1/thetas[x[1]][idx],0.0,0.0,0.0]
     #     elseif(idx == 2)
-    #         return [-1/thetas[x[1]][idx],1/thetas[x[1]][idx],-1/thetas[x[1]][idx],-1/thetas[x[1]][idx]]
+    #         return [0.0,1/thetas[x[1]][idx],0.0,0.0]
     #     elseif(idx == 3)
-    #         return [-1/thetas[x[1]][idx],-1/thetas[x[1]][idx],1/thetas[x[1]][idx],-1/thetas[x[1]][idx]]
+    #         return [0.0,0.0,1/thetas[x[1]][idx],0.0]
     #     elseif(idx == 4)
-    #         return [-1/thetas[x[1]][idx],-1/thetas[x[1]][idx],-1/thetas[x[1]][idx],1/thetas[x[1]][idx]]
+    #         return [0.0,0.0,0.0,1/thetas[x[1]][idx]]
     #     end
-    # end
-    # function pgFunc(x)
-    #     a = getAIdx(x[2])
-    #     if a == 1
-    #         gradPolicy = [1 - exp(thetas[x[1]][a])/sum(exp.(thetas[x[1]])), -exp(thetas[x[1]][a])/sum(exp.(thetas[x[1]])), -exp(thetas[x[1]][a])/sum(exp.(thetas[x[1]])), -exp(thetas[x[1]][a])/sum(exp.(thetas[x[1]]))]
-    #     elseif a == 2
-    #         gradPolicy = [-exp(thetas[x[1]][a])/sum(exp.(thetas[x[1]])), 1 - exp(thetas[x[1]][a])/sum(exp.(thetas[x[1]])), -exp(thetas[x[1]][a])/sum(exp.(thetas[x[1]])), -exp(thetas[x[1]][a])/sum(exp.(thetas[x[1]]))]
-    #     elseif a == 3
-    #         gradPolicy = [-exp(thetas[x[1]][a])/sum(exp.(thetas[x[1]])), - exp(thetas[x[1]][a])/sum(exp.(thetas[x[1]])), 1 - exp(thetas[x[1]][a])/sum(exp.(thetas[x[1]])), -exp(thetas[x[1]][a])/sum(exp.(thetas[x[1]]))]
-    #     elseif a == 4
-    #         gradPolicy = [-exp(thetas[x[1]][a])/sum(exp.(thetas[x[1]])), -exp(thetas[x[1]][a])/sum(exp.(thetas[x[1]])), -exp(thetas[x[1]][a])/sum(exp.(thetas[x[1]])), 1 - exp(thetas[x[1]][a])/sum(exp.(thetas[x[1]]))]
-    #     else
-    #         throw(error("not a valid action"))
-    #     end
-    #     return gradPolicy
-    # end
+    # end #hahaha
+
+    function pgFunc(x)
+        a = getAIdx(x[2])
+        if a == 1
+            gradPolicy = [1 - exp(thetas[x[1]][a])/sum(exp.(thetas[x[1]])), -exp(thetas[x[1]][a])/sum(exp.(thetas[x[1]])), -exp(thetas[x[1]][a])/sum(exp.(thetas[x[1]])), -exp(thetas[x[1]][a])/sum(exp.(thetas[x[1]]))]
+        elseif a == 2
+            gradPolicy = [-exp(thetas[x[1]][a])/sum(exp.(thetas[x[1]])), 1 - exp(thetas[x[1]][a])/sum(exp.(thetas[x[1]])), -exp(thetas[x[1]][a])/sum(exp.(thetas[x[1]])), -exp(thetas[x[1]][a])/sum(exp.(thetas[x[1]]))]
+        elseif a == 3
+            gradPolicy = [-exp(thetas[x[1]][a])/sum(exp.(thetas[x[1]])), - exp(thetas[x[1]][a])/sum(exp.(thetas[x[1]])), 1 - exp(thetas[x[1]][a])/sum(exp.(thetas[x[1]])), -exp(thetas[x[1]][a])/sum(exp.(thetas[x[1]]))]
+        elseif a == 4
+            gradPolicy = [-exp(thetas[x[1]][a])/sum(exp.(thetas[x[1]])), -exp(thetas[x[1]][a])/sum(exp.(thetas[x[1]])), -exp(thetas[x[1]][a])/sum(exp.(thetas[x[1]])), 1 - exp(thetas[x[1]][a])/sum(exp.(thetas[x[1]]))]
+        else
+            throw(error("not a valid action"))
+        end
+        return gradPolicy
+    end
+
     start = time()
     s = RL.observe(env)
     a = policyEps(s)
@@ -206,25 +218,19 @@ function policyGrad_episode!(thetas,env,alpha,eps=0.1)
         push!(hist, sp)
     end
     # Update Thetas based on trajectory (tau)
-    step = 1
     for x in tau
-        totGrad = zeros(4)
-        for k in 1:step
-            totGrad += pgFunc(tau[k])
-        end
-        thetas[x[1]] += alpha*(rtogo)*totGrad
+        thetas[x[1]] += alpha*pgFunc(x)*rtogo
         rtogo -= x[3]
-        step += 1
     end
     return (hist=hist, thetas = copy(thetas), time=time()-start, policy = policy)
 end
 
 function policyGrad!(env, alpha = 0.2, eps = 0.05, n_episodes=100)
     episodes = []
-    thetas = Dict((s) => 420*ones(4) for s in RL.observations(env))
+    thetas = Dict((s) => 0.5*ones(4) for s in RL.observations(env))
     for i in 1:n_episodes
         RL.reset!(env)
-        tau = policyGrad_episode!(thetas, env, max(0.1, alpha*(1-i/(n_episodes))), max(eps, 1-i/(n_episodes)))
+        tau = policyGrad_episode!(thetas, env, max(0.025, alpha*(1-i/(n_episodes))), max(eps, 1-i/(n_episodes)))
         push!(episodes, tau)
     end
     
@@ -236,7 +242,8 @@ end
 numEps = 150_000
 # sarsa_episodes = sarsa!(HW4.gw, n_episodes=numEps)
 # episodes = Dict("SARSA"=>sarsa_episodes)
-policyGrad_episodes = policyGrad!(HW4.gw,0.3,0.1,numEps)
+policyGrad_episodes = policyGrad!(HW4.gw,0.1,0.05,numEps)
 episodes = Dict("Policy Gradient"=>policyGrad_episodes)
 # episodes = Dict("SARSA"=>sarsa_episodes, "Policy Gradient"=>policyGrad_episodes)
 plotEnv(HW4.gw,episodes,numEps)
+plotEnvTime(HW4.gw,episodes,numEps)
